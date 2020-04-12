b63f964f5018b6bfd2468cf474ce63d49f3fddd6
Bashorun97 <bashorun.emma@gmail.com>
2020-04-12 22:32:07 +0100

    simple recursion applications

---
+++ recursion/PowerRecursion/PowerRecursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int power(int m, int n)
+{
+	if(n==0)
+		return 1;
+	return power(m,n-1)*m;
+}
+
+int power1(int m, int n)
+{
+	if(n==0)
+		return 1;
+	if(n%2==0)
+		return power1(m*m, n/2);
+	return m * power1(m*m,  (n-1)/2);
+}
+
+
+int main()
+{
+	int r=power(3, 4);
+	printf("%d", r);
+}
+
