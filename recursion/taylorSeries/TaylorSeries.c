ab8eb42bcef6230fb002dac91fa6344208df8abe
Bashorun97 <bashorun.emma@gmail.com>
2020-04-13 23:10:31 +0100

    Taylor series using Horners rule

---
+++ recursion/taylorSeries/TaylorSeries.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//Using Horners rule
+
+double e(int x, int n)
+{
+	static double s;
+
+	if (n==0)
+		return s;
+	s=1+x*s/n;
+	return e(x, n-1);
+}
+
+int main()
+{
+	printf("%lf \n", e(2,10));
+	return 0;
+}
+
