0834906e3c92fc572584e8b25c408b6f19a0ec48
Bashorun97 <bashorun.emma@gmail.com>
2020-06-30 20:25:20 +0100

    Tridiagonal

--- Matrices/TriDiagonal.c
+++ Matrices/TriDiagonal.c
@@ -5,23 +5,16 @@ struct Matrix
 {
   int *A;
   int n;
-  int length;
 };
 
 void Set(struct Matrix *m, int i, int j, int x)
 {
   if (i-j==1)
-  {
     m->A[i-2]=x;
-  }
   if (i-j==0)
-  {
     m->A[m->n-1+i-1]=x;
-  }
   if (i-j==-1)
-  {
     m->A[2*m->n-1+i-1]=x;
-  }
 }
 
 int Get(struct Matrix m, int i,int j)
@@ -38,27 +31,30 @@ int Get(struct Matrix m, int i,int j)
 void Display(struct Matrix m)
 {
   int i,j;
-  for (i==1;i<=m.n;i++)
+  for (i=1;i<=m.n;i++)
   {
-    for (j==1;j<=m.n;j++)
+    for (j=1;j<=m.n;j++)
     {
       if (i-j==1)
-      {
         printf("%d ", m.A[i-2]);
-      }
       else if (i-j==0)
-      {
         printf("%d ", m.A[m.n-1+i-1]);
-      }
       else if (i-j==-1)
-      {
         printf("%d ", m.A[2*m.n-1+i-1]);
-      }
       else
         printf("0 ");
     }
     printf("\n");
   }
+}
+
+int ListArray(struct Matrix m)
+{
+  int i,length;
+  length = 3*m.n-2;
+  for(i=0;i<length;i++)
+    printf("%d ", m.A[i]);
+  return 0;
 }
 
 int main()
@@ -80,6 +76,8 @@ int main()
   }
   printf("\n\n");
   Display(m);
+  ListArray(m);
+  printf("\nItem at position M[%d,%d]: %d\n",2,1,Get(m,2,1));
   return 0;
 }
 
